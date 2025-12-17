ba8858b560ddbcf8461bf99075ccb0c21ecb0c5c
JoshIBarrett <jibarrett@jeloba.com>
2025-12-17 10:11:47 -0800

    implement basic motors and copy pasted drivetrain (needs to be replaced with new constants file and maybe new drivetrain file)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,17 +4,84 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+  public TalonFX motor1;
+  public TalonFX motor2;
+  public CommandXboxController controller = new CommandXboxController(0);
 
-  private final RobotContainer m_robotContainer;
+  public CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
+  private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(Units.MetersPerSecond); // kSpeedAt12Volts desired top speed
+    private double MaxAngularRate = Units.RotationsPerSecond.of(0.75).in(Units.RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+
+  
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+  .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+  .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+
+
+  public TalonFXConfiguration getBasicConfig() {
+    TalonFXConfiguration config = new TalonFXConfiguration();
+    config.CurrentLimits.StatorCurrentLimit = 80.0;
+    config.CurrentLimits.SupplyCurrentLimit = 60.0;
+    config.CurrentLimits.SupplyCurrentLowerTime = 0;
+    return config;
+  }
+
+
+  public Command voltageCommand(TalonFX motor, double volts) {
+    return Commands.runOnce(() -> {
+      motor.setControl(new VoltageOut(volts));
+    });
+  }
+
+  public void configureBindings() {
+    controller.a().onTrue(voltageCommand(motor1, 2));
+    controller.a().onTrue(voltageCommand(motor1, -2));
+
+    controller.a().onTrue(voltageCommand(motor2, 4));
+    controller.a().onTrue(voltageCommand(motor2, -4));
+
+    drivetrain.setDefaultCommand(
+            // Drivetrain will execute this command periodically
+            drivetrain.applyRequest(() ->
+                drive.withVelocityX(-controller.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(-controller.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(-controller.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+            )
+        );
+  }
+  
 
   public Robot() {
-    m_robotContainer = new RobotContainer();
+    motor1 = new TalonFX(10, "rio");
+
+    TalonFXConfiguration motor1Config = getBasicConfig();
+
+    motor1.getConfigurator().apply(motor1Config);
+
+
+
+    motor2 = new TalonFX(11, "rio");
+
+    TalonFXConfiguration motor2Config = getBasicConfig();
+
+    motor2.getConfigurator().apply(motor2Config);
+
+    configureBindings();
   }
 
   @Override
@@ -32,13 +99,7 @@ public class Robot extends TimedRobot {
   public void disabledExit() {}
 
   @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
+  public void autonomousInit() {}
 
   @Override
   public void autonomousPeriodic() {}
@@ -47,11 +108,7 @@ public class Robot extends TimedRobot {
   public void autonomousExit() {}
 
   @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
+  public void teleopInit() {}
 
   @Override
   public void teleopPeriodic() {}
